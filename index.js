3a547381eb0a6419006b079138ef452a954123bb
Alex Edwards <alextcedwards@gmail.com>
2021-08-05 15:41:38 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("testing");
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json())
+server.use(cors());
+server.use(helmet());
+
+server.use("*", (req, res, next) => {
+  res.json({
+    message: "web 44 is awesome indeed",
+  });
+});
+
+
+
+server.listen(5000, () => {
+    console.log(`listening on ${5000}`)
+})
